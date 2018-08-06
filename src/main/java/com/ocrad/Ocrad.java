6a0f9d7f12dba8a5a539cf8b796b0e013de17c6a
pankaj kumar tumpala <pankaj kumar tumpala>
2018-08-06 08:27:46 +0530

    set image

--- src/main/java/com/ocrad/Ocrad.java
+++ src/main/java/com/ocrad/Ocrad.java
@@ -17,7 +17,18 @@
 
 package com.ocrad;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.PixelGrabber;
+import java.awt.image.WritableRaster;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
+import javax.imageio.ImageIO;
 
 public class Ocrad {
 	static {
@@ -28,28 +39,118 @@ public class Ocrad {
 		ClassLoader loader = Ocrad.class.getClassLoader();
 		File file = new File(loader.getResource("test.pbm").getFile());
 
-		Main m = new Main();
-		long ptr = m.OCRAD_open();
-		System.out.println(m.OCRAD_version());
-		Pixmap pixmap = new Pixmap(new int[]{0,1,3},100,500,0);
-		System.out.println(m.OCRAD_set_image(ptr, pixmap, false));
-		m.OCRAD_set_image_from_file(ptr, file.getPath(), false);
-		int errorno = m.OCRAD_get_errno(ptr);
-		m.OCRAD_recognize(ptr, true);
-		// Get blocks
-		if (errorno == 0) {
-			int blocks = m.OCRAD_result_blocks(ptr);
-			int linenum;
-			for (int i = 0; i < blocks; i++) {
-				linenum = m.OCRAD_result_lines(ptr, i);
+		try {
+			Main m = new Main();
+			long ptr = m.OCRAD_open();
+			System.out.println(m.OCRAD_version());
+			BufferedImage myPicture;
+			myPicture = ImageIO
+					.read(new File("Image.png"));
+			//System.out.println(ppm(myPicture));
+			Pixmap pixmap = new Pixmap(ppm(myPicture), 73, 1575, 0);
+			System.out.println(m.OCRAD_set_image(ptr, pixmap, false));
+			//m.OCRAD_set_image_from_file(ptr, file.getPath(), false);
+			int errorno = m.OCRAD_get_errno(ptr);
+			m.OCRAD_recognize(ptr, true);
+			System.out.println(errorno);
+			// Get blocks
+			if (errorno == 0) {
+				int blocks = m.OCRAD_result_blocks(ptr);
+				int linenum;
+				for (int i = 0; i < blocks; i++) {
+					linenum = m.OCRAD_result_lines(ptr, i);
 
-				for (int j = 0; j < linenum; j++) {
-					System.out.println(m.OCRAD_result_line(ptr, i, j));
+					for (int j = 0; j < linenum; j++) {
+						 System.out.println(m.OCRAD_result_line(ptr, i, j));
+					}
 				}
 			}
+
+			m.OCRAD_close(ptr);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static int[] pbm(BufferedImage image) throws IOException {
+		int width = image.getWidth();
+		int height = image.getHeight();
+		PixelGrabber grabber = new PixelGrabber(image, 0, 0, width, height, false);
+		try {
+			grabber.grabPixels();
+		} catch (InterruptedException e) {
+			throw new IOException(".pbm: couldn't grab pixels from image !");
+		}
+		int bpl = ((width % 8) == 0) ? width >> 3 : (width + 8) >> 3; // bytes per line
+//	    String header="P4\n"+(bpl<<3)+" "+height+"\n";
+		String header = "P4\n" + width + " " + height + "\n";
+		// System.out.println(header.getBytes());
+		StringBuilder sb = new StringBuilder();
+		sb.append(header);
+		WritableRaster raster = image.getRaster();
+
+		int k = 0;
+		byte b;
+		int pixel;
+		int[] bitmap = new int[bpl * height];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; (x < width) && (k < bitmap.length);) {
+				b = 0; // all white
+				for (int bit = 7; (bit >= 0) && (x < width); bit--) {
+					pixel = raster.getSample(x++, y, 0);
+					if (pixel == 0) {
+						b |= (1 << bit);
+					} // if black set bit
+				}
+				bitmap[k++] = b;
+				System.out.println((int) b & 0xffb);
+			}
+		}
+		/*
+		 * for (int i = 0; i < bitmap.length; i++) sb.append((int) bitmap[i] & 0xff);
+		 */
+		return bitmap;
+	}
+
+	public static byte[] ppm(BufferedImage image) throws IOException {
+		int width = image.getWidth();
+		int height = image.getHeight();
+		int data[] = new int[width * height];
+		PixelGrabber grabber = new PixelGrabber(image, 0, 0, width, height, data, 0, width);
+		try {
+			grabber.grabPixels();
+		} catch (InterruptedException e) {
+			throw new IOException(".ppm: couldn't grab pixels from image !");
 		}
 
-		m.OCRAD_close(ptr);
+		String header = "P6\n" + width + " " + height + "\n255\n";
+
+		ColorModel model = grabber.getColorModel();
+
+		int k = 0;
+		int col;
+		byte[] bitmap = new byte[width * height * 3];
+		/*BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("Buff.ppm")));
+		StringBuilder sb = new StringBuilder();*/
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				col = data[y * width + x];
+				bitmap[k++] = (byte) model.getRed(col);
+				bitmap[k++] = (byte) model.getGreen(col);
+				bitmap[k++] = (byte) model.getBlue(col);
+				//byte[] b = { (byte) model.getRed(col), (byte)model.getGreen(col), (byte)model.getBlue(col) };
+				//System.out.println( new String(b,"ISO-8859-1") );
+				/*writer.write(new String(b,"ISO-8859-1"));
+				sb.append(new String(b,"ISO-8859-1"));*/
+			}
+		}
+		//writer.flush();
+		//writer.close();
+		//System.out.println(sb.toString().length());
+		/*for(int i=0;i<bitmap.length;i++)
+			System.out.println(bitmap[i]);*/
+		return bitmap;
 	}
 
 }
--- src/main/java/com/ocrad/Pixmap.java
+++ src/main/java/com/ocrad/Pixmap.java
@@ -1,47 +1,41 @@
 package com.ocrad;
+
 public class Pixmap {
-	private int data[];
+	private byte data[];
 	private int height;
 	private int width;
 	private int mode;
 	
-	public Pixmap(int data[], int height, int width, int mode) {
+	public Pixmap(byte[] data, int height, int width, int mode) {
+		super();
 		this.data = data;
 		this.height = height;
 		this.width = width;
 		this.mode = mode;
 	}
-
-	public int[] getData() {
+	public byte[] getData() {
 		return data;
 	}
-
-	public void setData(int data[]) {
+	public void setData(byte[] data) {
 		this.data = data;
 	}
-
 	public int getHeight() {
 		return height;
 	}
-
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
 	public int getWidth() {
 		return width;
 	}
-
 	public void setWidth(int width) {
 		this.width = width;
 	}
-
 	public int getMode() {
 		return mode;
 	}
-
 	public void setMode(int mode) {
 		this.mode = mode;
 	}
-
+	
 }
